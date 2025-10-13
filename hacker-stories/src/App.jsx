870abeda4d1faf6694a7434c343b36ce668a31a5
SuraNega <suranega43@gmail.com>
2025-10-13 09:51:46 +0300

    updated

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,12 +1,60 @@
 import './App.css';
 import List from './component/list';
 import Search from './component/Search';
+import React from 'react';
+import { useState } from 'react';
 
-const welcome = {
-  greeting: "Hello",
-  title: "React"
-};
+// const welcome = {
+//   greeting: "Hello",
+//   title: "React"
+// };
 
+
+  // function App(){
+//   return (
+//     <>
+//       <Search welcome={welcome} />
+//       <List list={stories} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+// const App = () => {
+// const stories = [
+//   {
+//     title: 'React',
+//     url: 'https://reactjs.dev/',
+//     author: 'Jordan Walke',
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,  
+//   },
+//   {
+//     title: 'Redux',
+//     url: 'https://redux.js.org/',
+//     author: 'Dan Abramov, Andrew Clark',
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+// ]; 
+// const [searchTerm, setSearchTerm] = React.useState('');
+// const handleSearch = (event) => {
+// setSearchTerm(event.target.value);
+// };
+// return (
+// <div>
+// <h1>My Hacker Stories</h1>
+// <Search onSearch={handleSearch} />
+// <hr />
+// <List list={stories} />
+// </div>
+// );
+// };
+
+const App = () => {
 const stories = [
   {
     title: 'React',
@@ -25,16 +73,25 @@ const stories = [
     objectID: 1,
   },
 ]; 
+const [searchTerm, setSearchTerm] = React.useState('');
+const handleSearch = (event) => {
+setSearchTerm(event.target.value);
+};
+const searchedStories = stories.filter(function (story) {
+return story.title.includes(searchTerm);
 
+});
+return (
+<div>
+<h1>My Hacker Stories</h1>
+<Search onSearch={handleSearch} />
+<hr />
+<List list={searchedStories} />
+</div>
+);
+};
 
-
-function App(){
-  return (
-    <>
-      <Search welcome={welcome} />
-      <List list={stories} />
-    </>
-  );
-}
 
 export default App;
+
+
--- hacker-stories/src/component/Search.jsx
+++ hacker-stories/src/component/Search.jsx
@@ -1,52 +1,64 @@
-// const  Search = ({welcome}) => {
-//     // return(
-//     //         <div>
-//     //           <h1>{welcome.greeting} {welcome.title}</h1>
-//     // <label htmlFor = "search">Search</label>
-//     // <input id = "search" type = "text"/>
+// // const  Search = ({welcome}) => {
+// //     // return(
+// //     //         <div>
+// //     //           <h1>{welcome.greeting} {welcome.title}</h1>
+// //     // <label htmlFor = "search">Search</label>
+// //     // <input id = "search" type = "text"/>
 
-//     // <hr />
-
-
-
-//     //     </div>
-
-//     // )
-//     const handleChange = (event) => {
-//         console.log(event);
-//         console.log(event.target.value);
-//     };
+// //     // <hr />
 
 
-// return (
+
+// //     //     </div>
+
+// //     // )
+// //     const handleChange = (event) => {
+// //         console.log(event);
+// //         console.log(event.target.value);
+// //     };
+
+
+// // return (
+// //     <div>
+// //         <label htmlFor = "search">Search: </label>
+// //         <input id = "search" type = "text" onChange={handleChange}/>
+// //     </div>
+// // )
+// // };
+
+
+
+// import { useState } from 'react';
+
+// const Search = ({ onSearch }) => {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const handleChange = (event) => {
+//     setSearchTerm(event.target.value);
+//     onSearch(event);
+//   };
+
+//   return (
 //     <div>
-//         <label htmlFor = "search">Search: </label>
-//         <input id = "search" type = "text" onChange={handleChange}/>
+//       <label htmlFor="search">Search: </label>
+//       <input
+//         id="search"
+//         type="text"
+//         value={searchTerm}
+//         onChange={handleChange}
+//       />
+//       <p>
+//         Searching for <strong>{searchTerm}</strong>
+//       </p>
 //     </div>
-// )
+//   );
 // };
 
-
-
-import { useState } from 'react';
-
-const Search = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
-    </div>
-  );
-};
-
+const Search = (props) => (
+<div>
+<label htmlFor="search">Search: </label>
+<input id="search" type="text" onChange={props.onSearch} />
+</div>
+);
 
 export default Search;
